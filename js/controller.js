0fdf46412418c0c1e9421669daee225f69b079e1
Okada Tetsuhei <tetsuhei.okada.8920@gmail.com>
2020-07-20 15:32:20 +0900

    change top-image development

--- js/controller.js
+++ js/controller.js
@@ -21,4 +21,23 @@ $(function(){
         var pos = $("#apply").offset().top -50;
         $("html,body").animate({scrollTop:pos},600);
     });
+
+    $('#top-image .top-smp').on('click', function(){
+        var dt = $(this).attr('data');
+        var new_src = '';
+        if(dt == "1"){
+            new_src = $(this).attr("src").replace('top-smp', 'top-smp2');
+            $(this).attr("data","2");
+        }else if(dt == "2"){
+            new_src = $(this).attr("src").replace('top-smp2', 'top-smp3');
+            $(this).attr("data","3");
+        }else if(dt == "3"){
+            new_src = $(this).attr("src").replace('top-smp3', 'top-smp4');
+            $(this).attr("data","4");
+        }else{
+            new_src = $(this).attr("src").replace('top-smp4', 'top-smp');
+            $(this).attr("data","1");
+        }
+        $(this).attr("src", new_src);
+    });
 });
