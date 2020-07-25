4a09a6b1287d54361d16b2ba1a9957daf8c73178
Okada Tetsuhei <tetsuhei.okada.8920@gmail.com>
2020-07-26 03:35:26 +0900

    some adjusts

--- js/controller.js
+++ js/controller.js
@@ -15,14 +15,14 @@ $(function(){
     });
     $(".smpMenu li").on("click", function(){
         var _attr = $(this).attr("data");
-        var scroll_top = $(_attr).offset().top - 50;
+        var scroll_top = $(_attr).offset().top - 10;
         $("html,body").animate({scrollTop:scroll_top},600);
 
         $(".menu_btn").removeClass('open');
         $(".smpMenu").removeClass('open')
     });
     $("#apply_btn, #contact_btn").on("click", function(){
-        var pos = $("#apply").offset().top -50;
+        var pos = $("#apply").offset().top -10;
         $("html,body").animate({scrollTop:pos},600);
     });
     $("a span.phone").on("click",function(){
