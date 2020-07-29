15f83196e329bbe9fb1cc987b55c13a5f4f27ec1
Okada Tetsuhei <tetsuhei.okada.8920@gmail.com>
2020-07-29 09:50:55 +0900

    php making

--- js/controller.js
+++ js/controller.js
@@ -33,30 +33,15 @@ $(function(){
         }
     });
 
-    // $('#top-image .top-smp').on('click', function(){
-    //     var dt = $(this).attr('data');
-    //     var new_src = '';
-    //     if(dt == "1"){
-    //         new_src = $(this).attr("src").replace('top-smp', 'top-smp2');
-    //         $(this).attr("data","2");
-    //     }else if(dt == "2"){
-    //         new_src = $(this).attr("src").replace('top-smp2', 'top-smp3');
-    //         $(this).attr("data","3");
-    //     }else if(dt == "3"){
-    //         new_src = $(this).attr("src").replace('top-smp3', 'top-smp4');
-    //         $(this).attr("data","4");
-    //     }else{
-    //         new_src = $(this).attr("src").replace('top-smp4', 'top-smp');
-    //         $(this).attr("data","1");
-    //     }
-    //     $(this).attr("src", new_src);
-    // });
 
-    $("form .btn").on("click", function(){
+    $("form #send").on("click", function(){
         var errMsg1 = "この項目を空欄にすることはできません。";
         var errMsg2 = "数字以外の入力はできません。";
-        var errMsg3 = "メールアドレスを正しく入力してください。";
-        var errMsg4 = "500文字以内で入力してください。";
+        var errMsg3 = "18歳未満の応募はできません。";
+        var errMsg4 = "メールアドレスを正しく入力してください。";
+        var errMsg5 = "500文字以内で入力してください。";
+        var errMsg6 = "30文字以内で入力してください。";
+        var errMsg7 = "電話番号は10桁か11桁の半角数字を入力してください。";
         var isErr = false;
 
         var $age = $("form input[name='age']");
@@ -65,25 +50,137 @@ $(function(){
         var $mail = $("form input[name='mail']");
         var $inquery = $("form textarea[name='inquery']");
 
+        // 年齢のチェック
         if(isNull_chk($age.val())){
+            // 必須チェック
             $age.next().text(errMsg1);
+            $age.next().show();
+            isErr = true;
+        }else if(isNumeric_chk($age.val())){
+            //数字チェック
+            $age.next().text(errMsg2);
+            $age.next().show();
+            isErr = true;
+        }else if(parseInt($age.val()) < 19){
+            // 年齢チェック
+            $age.next().text(errMsg3);
+            $age.next().show();
             isErr = true;
         }
-        if(isNull_chk($name.val())){
-            $name.next().text(errMsg1);
-            isErr = true;
-        }
-        if(isNull_chk($phone.val())){
-            $phone.next().text(errMsg1);
-            isErr = true;
+        else{
+            $age.next().hide();
         }
 
+        // 名前のチェック
+        if(isNull_chk($name.val())){
+            // 必須チェック
+            $name.next().text(errMsg1);
+            $name.next().show();
+            isErr = true;
+        }else if($.trim($name.val()).length > 30){
+            // 文字数チェック
+            $name.next().text(errMsg6);
+            $name.next().show();
+            isErr = true;
+        }else{
+            $name.next().hide();
+        }
+        // 電話番号のチェック
+        if(isNull_chk($phone.val())){
+            //　必須チェック
+            $phone.next().text(errMsg1);
+            $phone.next().show();
+            isErr = true;
+        }else if(isNumeric_chk($phone.val())){
+            //　数字チェック
+            $phone.next().text(errMsg2);
+            $phone.next().show();
+            isErr = true;
+        }else if($.trim($phone.val()).length < 10 || $.trim($phone.val()).length > 11){
+            // 桁数チェック
+            $phone.next().text(errMsg7);
+            $phone.next().show();
+            isErr = true;
+        }else{
+            $phone.next().hide();
+        }
+        // メールのチェック
+        if(mail_chk($mail.val())){
+            $mail.next().text(errMsg4);
+            $mail.next().show();
+            isErr = true;
+        }else{
+            $mail.next().hide();
+        }
+        // 問い合わせ内容のチェック
+        var inquery_str = $inquery.val();
+        if($.trim(inquery_str).length > 0 && $.trim(inquery_str).length > 500){
+            $inquery.next().text(errMsg5 + "[" + $.trim(inquery_str).length + " /500]");
+            $inquery.next().show();
+            isErr = true;
+        }else{
+            $inquery.next().hide();
+        }
+
+        if(isErr){
+            return false;
+        }else{
+            $age.next().hide();
+            $name.next().hide();
+            $phone.next().hide();
+            $mail.next().hide();
+            $("#send").next().hide();
+
+            $.ajax({
+                type: "POST",
+                url: "./php/sendmail.php",
+                data: {
+                    'age' : $age.val(),
+                    'name' : $name.val(),
+                    'phone' : $phone.val(),
+                    'mail' : $mail.val(),
+                    'inquery' : $inquery.val()
+                }
+            }).done(function(data, dataType) {
+                $("#send").next().text(data.msg);
+                $("#send").next().show();
+            }).fail(function(XMLHttpRequest, textStatus, errorThrown) {
+                $("#send").next().text("送信に失敗しました。時間を置いて再度送信しても解決しない場合は、別の方法でお問い合わせください。");
+                $("#send").next().show();
+            });
+            return false;
+        }
     });
 
     function isNull_chk(val){
         if(val == null){
             return true;
         }else if($.trim(val).length == 0){
+            return true;
+        }
+        return false;
+    }
+    function isNumeric_chk(val){
+        for(var i = 0; i < val.length -1; i++){
+            var num = parseInt(val.charAt(i),10);
+            if((!num || isNaN(num)) && num !== 0){
+                return true;
+            }
+        }
+
+        return false;
+    }
+    function mail_chk(val){
+        var mail_str = val == null || val == undefined ? "" : $.trim(val);
+        if(mail_str.length == 0 ){
+            return false;
+        }else if(mail_str.length < 5){
+            return true;
+        }
+        
+        if(mail_str.indexOf('@', 0) == -1){
+            return true;
+        }else if(mail_str.match(/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/) == null){
             return true;
         }
         return false;
--- php/sendmail.php
+++ php/sendmail.php
@@ -1,32 +1,87 @@
 <?php
+header("Content-type: text/plain; charset=UTF-8");
+$is_err_flg = false;
+if(isset($_SERVER['HTTP_X_REQUESTED_WITH'])
+   && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest')
+{
+    $age = isset($_POST['age']) ? $_POST['age'] : NULL;
+    $name = isset($_POST['name']) ? $_POST['name'] : NULL;
+    $phone = isset($_POST['phone']) ? $_POST['phone'] : NULL;
+    $mail = isset($_POST['mail']) ? $_POST['mail'] : NULL;
+    $inquery = isset($_POST['inquery']) ? $_POST['inquery'] : NULL;
 
-$age = isset($_POST['age']) ? $_POST['age'] : NULL;
-$name = isset($_POST['name']) ? $_POST['name'] : NULL;
-$phone = isset($_POST['phone']) ? $_POST['phone'] : NULL;
-$mail = isset($_POST['mail']) ? $_POST['mail'] : NULL;
-$inquery = isset($_POST['inquery']) ? $_POST['inquery'] : NULL;
-$is_submit = isset($_POST['submitted']) ? true : false;
-
-if($is_submit){
+    if(is_null($age) || is_null($name) || is_null($phone)){
+        $is_err_flg = true;
+    }else if(is_null($mail)){
+        $mail = "";
+    }else if(is_null($inquery)){
+        $inquery = "";
+    }
 
 }else{
-
+    return false;
 }
 
-$mail_to = "celinelivechat@gmail.com";
-$mail_title = "WEBフォームからのお問い合わせ";
-$mail_body = "WEBフォームからお問い合わせがありました。";
-$mail_body .= "/n/n"."お名前/n".$name;
-$mail_body .= "/n/n"."年齢/n".$age;
-$mail_body .= "/n/n"."電話番号/n".$phone;
-if($mail){
-    $mail_body .= "/n/n"."メールアドレス/n".$mail;
+// お問い合わせメール送信
+$to = "tetsu.o.8920@gmail.com";
+// $to = "celinelivechat@gmail.com";
+$subject = "WEBフォームからのお問い合わせ";
+$message = "WEBフォームからお問い合わせがありました。";
+$message .= "\r\n\r\n[お名前]\r\n".$name;
+$message .= "\r\n\r\n[年齢]\r\n".$age;
+$message .= "\r\n\r\n[電話番号]\r\n".$phone;
+if(strlen($mail) > 0){
+    $message .= "/\r\n\r\n[メールアドレス]\r\n".$mail;
 }
-if($inquery){
-    $mail_body .= "/n/n"."お問い合わせ内容/n".$inquery;
+if(strlen($inquery) > 0){
+    $message .= "/\r\n\r\n[お問い合わせ内容]\r\n".$inquery;
+}
+$headers = "From: tetsuhei.okada.8920@gmail.com";
+// $headers = "From: celinelivechat@gmail.com";
+mb_language("ja");
+mb_internal_encoding("UTF-8");
+$is_send_mail_flg = false;
+if(mb_send_mail($to, $subject, $message, $headers)){
+    //送信成功
+    $is_send_mail_flg = true;
+    echo json_encode(
+        array(
+            "result"=>true,
+            "msg"=>"送信しました。お問い合わせありがとうございます。"
+        )
+    );
+}else{
+    //送信失敗
+    echo json_encode(
+        array(
+            "result"=>false,
+            "msg"=>"メールの送信に失敗しました。入力内容をご確認のうえ、再度送信してください。"
+        )
+    );
 }
 
-
-
+// メールアドレスが記入されている場合、その人に確認メールを送る
+if(strlen($mail) > 0){
+    $to = $mail;
+    $from = "<tetsuhei.okada.8920@gmail.com>";
+    $subject = "[celine.]　お問い合わせありがとうございます。";
+    $message = $name." 様\r\n\r\n";
+    $message += "お問い合わせいただき、ありがとうございます。\r\n";
+    $message += "2−3営業日中に担当の者からお電話またはメールにてご連絡させていただきます。\r\n";
+    $message += "今しばらくお待ちくださいませ。\r\n\r\n";
+    $message += "なお、返信がない場合は大変お手数ですが再度お問い合わせいただきますよう\r\n";
+    $message += "お願い申し上げます。\r\n\r\n\r\n";
+    if(strlen($inquery) > 0){
+        $message += "[お問い合わせ内容]\r\n";
+        $message += $inquery;
+        $message += "\r\n\r\n\r\n";
+    }
+    $message += "celine. [セリーヌ]";
+    
+    $headers = "From:".mb_encode_mimeheader("celine | セリーヌ").$from;
+    mb_language("ja");
+    mb_internal_encoding("UTF-8");
+    mb_send_mail($to, $subject, $message, $headers)
+}
 
 ?>
